2c393c4e0e9fb30d7c1490cb08952f7a162d660d
César <cesardsbraz@gmail.com>
2025-05-08 23:21:52 -0300

    add product routes

--- backend/src/routes/product.routes.ts
+++ backend/src/routes/product.routes.ts
@@ -4,5 +4,8 @@ import * as productController from "../controllers/product.controller"
 const router = Router()
 
 router.get("/", productController.getAllProducts)
+router.post("/", productController.createProduct)
+router.get("/:id", productController.getProductById)
+router.put("/:id", productController.updateProduct)
 
 export default router
