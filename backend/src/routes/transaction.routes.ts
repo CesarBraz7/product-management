7a52d1ebdbfaa1f236e8a18c402ca1b21433662f
César <cesardsbraz@gmail.com>
2025-05-08 23:24:00 -0300

    add transaction routes

--- backend/src/routes/transaction.routes.ts
+++ backend/src/routes/transaction.routes.ts
@@ -4,5 +4,9 @@ import * as transactionController from '../controllers/transaction.controller'
 const router = Router()
 
 router.get('/', transactionController.getAllTransactions)
+router.post('/', transactionController.createTransaction)
+router.get('/:id', transactionController.getTransactionById)
+router.get('/product/:id', transactionController.getTransactionByProductId)
+router.get('/date/:date', transactionController.getTransactionByDate)
 
 export default router
